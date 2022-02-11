09e9c10a710ec0ca0cc5e571367d691e30a9af6d
suxrobshukurov <sdead96@gmail.com>
2022-02-11 17:55:05 +0300

    added block slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
+const { default: Swiper } = require("swiper");
+
+// меню бургер
 jQuery(function() {
     jQuery('.burger').click(function() {
         jQuery('.burger span').toggleClass("active");
         jQuery('ul.nav-list').toggleClass("animate");
+    });
+
+    // слайдер
+
+    var swiper = new Swiper('.slider', {
+        pagination: {
+            el: '.swiper-pagination',
+        },
     });
 });
